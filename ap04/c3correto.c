5a61fb3bf9c51cbfc280b6369141e1656cb0b37e
Yuri Wada <yuri.wadah@gmail.com>
2024-11-15 15:24:03 -0300

    Fix Ap04 C4

---
+++ ap04/c3correto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+  int i;
+  int *a[10];
+
+  for (i=0; i < 10; i++)
+  {
+    a[i] = malloc(sizeof(int) * 100);
+  }
+  
+  for (i = 0; i < 10; i++)
+  {
+    free(a[i]);
+  }
+  
+  return 0;
+}
+
