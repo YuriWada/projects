c1ea636edcfacba489abc16668756c53b7c7d1c9
Yuri Wada <yuri.wadah@gmail.com>
2024-11-15 20:55:15 -0300

    C5

--- ap04/c1correto.c
+++ ap04/c1correto.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv){
-  int i;
-  int *a = malloc(sizeof(int) * 11);
-  if (!a) return -1; /*malloc failed*/
-  for (i = 0; i < 11; i++){
-    a[i] = i;
-  }
-  free(a);
-  return 0;
+    int i;
+    int *a = malloc(sizeof(int) * 11);
+    
+    if (!a) return -1; /*malloc failed*/
+    
+    for (i = 0; i < 11; i++)
+    {
+      a[i] = i;
+    
+    }
+    
+    free(a);
+  
+    return 0;
 }
